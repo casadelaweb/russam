1369121be64d72f5d7544b9dd5b7d5aed3d38534
Alex Volkov <wreckweighted@gmail.com>
2024-11-30 14:23:42 +0300

    refactor: remove obsolete config files

--- configuration/webpack/html-loader-pipeline/process-repeat.js
+++
@@ -1,8 +0,0 @@
-export default function processRepeat(match, number, content) {
-  // const occurrences = content.split('end;').length
-
-
-  const times = Number(number) | 1
-  match = content.repeat(times)
-  return match
-}
